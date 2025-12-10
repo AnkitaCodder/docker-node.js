ecd9c599ad986628ce3502e37e45ee89493e62b6
Ankita Mardana <119393582+AnkitaCodder@users.noreply.github.com>
2025-12-10 19:19:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const express = require ("express");
 const app = express();
 
-const PORT = ProcessingInstruction.env.PORT || 8000;
+const PORT = Process.env.PORT || 8000;
 
-app.length("/", (req,res)=> {
+app.get("/", (req,res)=> {
     return res.json ({message : "Hey, I am nodejs in container running by ankita"});
 });
+
 
 app.listen (PORT,() => console.log(`Server started on PORT:${PORT}`))
